a4d54c148f038fdbce89eefa5ba478066fcd27ea
eastmeet <ys932184@gmail.com>
2023-06-02 16:38:19 +0900

    FEAT: Member Entity 및 MemberDto 구현

--- fin-point/src/main/java/com/fp/finpoint/domain/member/entity/Member.java
+++ fin-point/src/main/java/com/fp/finpoint/domain/member/entity/Member.java
@@ -1,4 +1,25 @@
 package com.fp.finpoint.domain.member.entity;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    private String email;
+
+    private String password;
+
+    private String salt;
 }
