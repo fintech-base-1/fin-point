11406a221a90e8cbc4924df5cc7fdbd0b5874694
eastmeet <ys932184@gmail.com>
2023-06-02 16:39:43 +0900

    TEST: Member 회원가입 및 로그인 테스트

---
+++ fin-point/src/test/java/com/fp/finpoint/memberTest/MemberTest.java
@@ -0,0 +1,47 @@
+package com.fp.finpoint.memberTest;
+
+import com.fp.finpoint.domain.member.dto.MemberDto;
+import com.fp.finpoint.domain.member.entity.Member;
+import com.fp.finpoint.domain.member.repository.MemberRepository;
+import com.fp.finpoint.domain.member.service.MemberService;
+import com.fp.finpoint.util.PasswordEncoder;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MemberTest {
+    @Autowired
+    private MemberService memberService;
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    void memberPasswordTest() {
+        String email = "haha@naver.com";
+        String password = "hahahoho!123";
+        String password1 = "hahahoho!124";
+
+        MemberDto memberDto = new MemberDto();
+        memberDto.setEmail(email);
+        memberDto.setPassword(password);
+        memberService.registerMember(memberDto);
+        Member savedMember = memberRepository.findByEmail(email).orElseThrow(
+                ()-> new RuntimeException("error"));
+
+//        Assertions.assertThat(savedMember.getEmail()).isEqualTo(email);
+//        Assertions.assertThat(savedMember.getPassword()).isEqualTo(password);
+
+        String s = PasswordEncoder.hashPassword(password, savedMember.getSalt());
+        System.out.println(s);
+        String s1 = PasswordEncoder.hashPassword(password1, savedMember.getSalt());
+//        Assertions.assertThat(password1()).isEqualTo(s);
+//        Assertions.assertThat(s).isEqualTo(s트1);
+
+        memberService.doLogin(memberDto);
+
+//        Assertions.assertThat()
+//        PasswordEncoder.hashPassword(password, )
+
+    }
+}
