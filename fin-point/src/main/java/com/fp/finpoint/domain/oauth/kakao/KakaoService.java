dc9ce9902195b2bad87b66d740f552571fd3c932
Damgom <wisewish1@gmail.com>
2023-06-03 17:17:00 +0900

    FEAT: simple oauth join logic

--- fin-point/src/main/java/com/fp/finpoint/domain/oauth/kakao/KakaoService.java
+++ fin-point/src/main/java/com/fp/finpoint/domain/oauth/kakao/KakaoService.java
@@ -1,5 +1,7 @@
 package com.fp.finpoint.domain.oauth.kakao;
 
+import com.fp.finpoint.domain.member.entity.Member;
+import com.fp.finpoint.domain.member.repository.MemberRepository;
 import com.fp.finpoint.domain.oauth.feign.KakaoLoginFeign;
 import com.fp.finpoint.domain.oauth.feign.KakaoGetProfileFeign;
 import com.fp.finpoint.web.oauth.dto.KakaoProfileResponseDto;
@@ -16,6 +18,7 @@ public class KakaoService {
 
     private final KakaoLoginFeign kakaoLoginFeign;
     private final KakaoGetProfileFeign kakaoGetProfileFeign;
+    private final MemberRepository memberRepository;
 
     @Value("${oauth.kakao.client_id}")
     private String client_id;
@@ -31,7 +34,15 @@ public class KakaoService {
         log.info("accessToken = {}" , accessToken);
         KakaoProfileResponseDto kakaoProfileResponseDto = kakaoGetProfileFeign.getProfile(accessToken, "application/x-www-form-urlencoded;charset=utf-8");
         log.info("email = {}", kakaoProfileResponseDto.getKakao_account().getEmail());
+        oauthJoin(kakaoProfileResponseDto.getKakao_account().getEmail());
         return kakaoProfileResponseDto;
     }
 
+    public void oauthJoin(String email) {
+        if (memberRepository.findByEmail(email).isPresent()) {
+            return;
+        }
+        Member member = Member.builder().email(email).build();
+        memberRepository.save(member);
+    }
 }
