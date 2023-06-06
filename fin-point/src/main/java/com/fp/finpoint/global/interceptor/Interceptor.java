f55591992e7bef3f18810a26a86f558a989d59fe
Damgom <wisewish1@gmail.com>
2023-06-06 11:34:25 +0900

    INIT: handler interceptor implementation

--- fin-point/src/main/java/com/fp/finpoint/global/interceptor/Interceptor.java
+++ fin-point/src/main/java/com/fp/finpoint/global/interceptor/Interceptor.java
@@ -1,4 +1,32 @@
 package com.fp.finpoint.global.interceptor;
 
-public class Interceptor {
+import com.fp.finpoint.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+@RequiredArgsConstructor
+public class Interceptor implements HandlerInterceptor {
+    //todo: interceptor 구현
+    private final MemberRepository memberRepository;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
 }
