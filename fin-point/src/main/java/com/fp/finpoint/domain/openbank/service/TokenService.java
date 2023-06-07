f659341d4986e92b0ebb80cc2d0b17d5f305d7e3
Damgom <wisewish1@gmail.com>
2023-06-07 16:44:41 +0900

    REFACTOR: delete not necessary method and rename method

--- fin-point/src/main/java/com/fp/finpoint/domain/openbank/service/TokenService.java
+++ fin-point/src/main/java/com/fp/finpoint/domain/openbank/service/TokenService.java
@@ -4,7 +4,6 @@ import com.fp.finpoint.domain.member.entity.Member;
 import com.fp.finpoint.domain.member.repository.MemberRepository;
 import com.fp.finpoint.domain.openbank.BankingFeign;
 import com.fp.finpoint.domain.openbank.Entity.Token;
-import com.fp.finpoint.domain.openbank.repository.TokenRepository;
 import com.fp.finpoint.global.exception.BusinessLogicException;
 import com.fp.finpoint.global.exception.ExceptionCode;
 import com.fp.finpoint.global.util.CookieUtil;
@@ -24,7 +23,6 @@ import java.io.UnsupportedEncodingException;
 @Slf4j
 public class TokenService {
 
-    private final TokenRepository tokenRepository;
     private final BankingFeign bankingFeign;
     private final MemberRepository memberRepository;
 
@@ -61,8 +59,7 @@ public class TokenService {
 
     public void setTokenToMember(Token token, HttpServletRequest request) throws UnsupportedEncodingException {
         String email = CookieUtil.getEmailToCookie(request);
-        Member savedMember = memberRepository.findByEmail(email)
-                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+        Member savedMember = getMember(email);
         savedMember.setToken(token);
         memberRepository.save(savedMember);
     }
@@ -70,19 +67,23 @@ public class TokenService {
     @Transactional
     public void getAccountList(HttpServletRequest request) throws UnsupportedEncodingException {
         String email = CookieUtil.getEmailToCookie(request);
-        Member savedMember = memberRepository.findByEmail(email)
-                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+        Member savedMember = getMember(email);
         Token savedToken = savedMember.getToken();
-        String token = createToken(savedToken.getToken_type(), savedToken.getAccess_token());
+        String bankToken = createBankToken(savedToken.getToken_type(), savedToken.getAccess_token());
         AccountResponseDto accountResponseDto =
-                bankingFeign.getAccountList(token, savedToken.getUserSeqNo(), "N", "D");
+                bankingFeign.getAccountList(bankToken, savedToken.getUserSeqNo(), "N", "D");
         // 등록된 계좌중 첫번째 계좌의 Fintech_use_num 을 가져와서 저장
         String finUseNum = accountResponseDto.getRes_list().get(0).getFintech_use_num();
         savedMember.setFintech_use_num(finUseNum);
         log.info("fin_use_num={}", savedMember.getFintech_use_num());
     }
 
-    public String createToken(String type, String accessToken) {
+    private Member getMember(String email) {
+        return memberRepository.findByEmail(email)
+                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+    }
+
+    private String createBankToken(String type, String accessToken) {
         StringBuilder sb = new StringBuilder();
         sb.append(type).append(" ").append(accessToken);
         return sb.toString();
