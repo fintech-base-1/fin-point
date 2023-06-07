974a3859dc86758cda6155b638069dbcc2d6b9f7
hojncode <glicm12@gmail.com>
2023-06-07 16:29:52 +0900

    REFACTOR : 불필요 어노테이션 삭제

--- fin-point/src/main/java/com/fp/finpoint/domain/invest/repository/InvestRepository.java
+++ fin-point/src/main/java/com/fp/finpoint/domain/invest/repository/InvestRepository.java
@@ -5,10 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-@Transactional
 public interface InvestRepository extends JpaRepository<Invest, Long> {
 
     Page<Invest> findBySubjectContaining(String searchKeyword, Pageable pageable);
