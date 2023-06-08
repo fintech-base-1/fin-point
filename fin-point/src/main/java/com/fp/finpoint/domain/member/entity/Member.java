b25a9a2bc4b1053e71eb8ee9b58d957e28ed181b
junghyun <jhyun9682@naver.com>
2023-06-08 17:17:43 +0900

    REFACTOR: 좋아요 수정

--- fin-point/src/main/java/com/fp/finpoint/domain/member/entity/Member.java
+++ fin-point/src/main/java/com/fp/finpoint/domain/member/entity/Member.java
@@ -1,5 +1,7 @@
 package com.fp.finpoint.domain.member.entity;
 
+//import com.fp.finpoint.domain.like.Like;
+import com.fp.finpoint.domain.like.Like;
 import com.fp.finpoint.domain.oauth.OauthClient;
 import com.fp.finpoint.domain.openbank.Entity.Token;
 import lombok.AllArgsConstructor;
@@ -10,7 +12,9 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -37,7 +41,8 @@ public class Member {
     @Enumerated(EnumType.STRING)
     private OauthClient oauthClient;
 
-    @ElementCollection(fetch = FetchType.EAGER)
+//    @ElementCollection(fetch = FetchType.EAGER)
+    @ElementCollection()
     @Enumerated(EnumType.STRING)
     private Set<Role> roles = new HashSet<>();
   
@@ -57,4 +62,9 @@ public class Member {
     public void setToken(Token token) {
         this.token = token;
     }
+
+
+    @OneToMany(mappedBy = "member")
+    private List<Like> likes = new ArrayList<>(); //좋아요
+
 }
--- fin-point/src/main/java/com/fp/finpoint/domain/member/service/MemberService.java
+++ fin-point/src/main/java/com/fp/finpoint/domain/member/service/MemberService.java
@@ -1,17 +1,27 @@
 package com.fp.finpoint.domain.member.service;
 
+import com.fp.finpoint.domain.invest.entity.DataNotFoundException;
 import com.fp.finpoint.domain.member.dto.MemberDto;
 import com.fp.finpoint.domain.member.entity.Member;
+import com.fp.finpoint.domain.member.entity.Role;
 import com.fp.finpoint.domain.member.repository.MemberRepository;
+import com.fp.finpoint.domain.oauth.OauthClient;
 import com.fp.finpoint.global.exception.BusinessLogicException;
 import com.fp.finpoint.global.exception.ExceptionCode;
-import com.fp.finpoint.util.EmailSenderService;
-import com.fp.finpoint.util.PasswordEncoder;
+import com.fp.finpoint.global.util.EmailSenderService;
+import com.fp.finpoint.global.util.JwtUtil;
+import com.fp.finpoint.global.util.PasswordEncoder;
+import com.fp.finpoint.global.util.RedisUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
-import javax.mail.MessagingException;
+
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Service
@@ -20,31 +30,82 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final EmailSenderService emailSenderService;
+    private final RedisUtil redisUtil;
 
     public void registerMember(MemberDto memberDto) {
         // 검증
-        verifyExistEmail(memberDto.getEmail());
+        isExistEmail(memberDto.getEmail());
 
-        // 암호화 처리
+        // 암호화
         String salt = PasswordEncoder.generateSalt();
         String password = PasswordEncoder.hashPassword(memberDto.getPassword(), salt);
 
-        // 저장
+        // 권한
+        Set<Role> roles = new HashSet<>();
+        roles.add(Role.ROLE_USER);
+
+        // 등록
         Member member = Member.builder()
                 .email(memberDto.getEmail())
                 .password(password)
                 .salt(salt)
+                .roles(roles)
+                .oauthClient(OauthClient.NOTHING)
                 .build();
 
         memberRepository.save(member);
         log.info("# Successful Member Registration!");
     }
 
-    public void doLogin(MemberDto memberDto) throws MessagingException {
-        Member member;
-        member = inspectEmailExistence(memberDto.getEmail());
-        verifyPassword(memberDto.getPassword(), member);
-        transferEmail(member);
+    public void oauthJoin(String email, OauthClient oauthClient) {
+        isExistEmail(email);
+        Set<Role> roles = new HashSet<>();
+        roles.add(Role.ROLE_USER);
+        Member member = Member.builder().
+                email(email)
+                .roles(roles)
+                .oauthClient(oauthClient)
+                .build();
+
+        memberRepository.save(member);
+    }
+
+    public void doLogin(MemberDto memberDto) {
+        Member savedMember = inspectEmailExistence(memberDto.getEmail());
+        OauthClient oauthClient = savedMember.getOauthClient();
+        switch (oauthClient) {
+            case NOTHING:
+                verifyPassword(memberDto.getPassword(), savedMember);
+                String verificationCode = transferEmail(savedMember);
+                setMemberInRedisWithCode(savedMember, verificationCode);
+                break;
+            case GOOGLE:
+                throw new BusinessLogicException(ExceptionCode.MEMBER_REGISTRY_GOOGEL);
+            case NAVER:
+                throw new BusinessLogicException(ExceptionCode.MEMBER_REGISTRY_NAVER);
+            case KAKAO:
+                throw new BusinessLogicException(ExceptionCode.MEMBER_REGISTRY_KAKAO);
+        }
+    }
+
+    public String checkCode(String code) {
+        ValueOperations<String, String> operations = redisUtil.getValueOperations();
+        String value = redisUtil.getRedisValue(operations, code);
+        isCodeExpired(value);
+        return value;
+    }
+
+    private static void isCodeExpired(String value) {
+        if (value == null) {
+            throw new BusinessLogicException(ExceptionCode.CODE_EXPIRED);
+        }
+    }
+
+    public void addSeller(String loginUserEmail) {
+        Member member = inspectEmailExistence(loginUserEmail);
+        Set<Role> roles = member.getRoles();
+        roles.add(Role.ROLE_SELLER);
+        memberRepository.save(member);
     }
 
     private Member inspectEmailExistence(String knockEmail) {
@@ -62,37 +123,40 @@ public class MemberService {
         }
     }
 
-    private void transferEmail(Member member) throws MessagingException {
-        String code = emailSenderService.sendHtmlMessageWithInlineImage(member.getEmail());
-        member.assignCode(code);
-        memberRepository.save(member);
-        log.info("# Member Code Save");
+    private String transferEmail(Member member) {
+        String email = member.getEmail();
+        String code = UUID.randomUUID().toString();
+        emailSenderService.sendHtmlMessageWithInlineImage(email, code);
+        log.info("# Authentication Mail Transfer!");
+        log.info("# Code = {}", code);
+        return code;
     }
 
-    private void verifyExistEmail(String email) {
+    private void setMemberInRedisWithCode(Member member, String code) {
+        String email = member.getEmail();
+        ValueOperations<String, String> operations = redisUtil.getValueOperations();
+        redisUtil.setRedisValue(operations, code, email, 10, TimeUnit.MINUTES);
+        log.info("# Code set in Redis!");
+    }
+
+    private void isExistEmail(String email) {
         memberRepository.findByEmail(email)
                 .ifPresent(member -> {
                     throw new BusinessLogicException(ExceptionCode.MEMBER_ALREADY_EXISTS);
                 });
     }
 
-    public Member checkCode(String code) {
-        Member member = memberRepository.findByCode(code).orElseThrow(
-                () -> new BusinessLogicException(ExceptionCode.MEMBER_WRONG_CODE)
-        );
-        log.info("Code !!");
+//    //좋아요 기능
+//    public Member getMember(Member token){
+//        Optional<Member> member=this.memberRepository.findByToken(token);
+//        if(member.isPresent()) {
+//            return member.get();
+//        }else{
+//            throw new DataNotFoundException("member not found");
+//        }
+//
+//    }
 
-        return member;
-    }
 
-    //좋아요 기능
-    public Member getMember(String email){
-        Optional<Member> member=this.memberRepository.findByEmail(email);
-        if(member.isPresent()) {
-            return member.get();
-        }else{
-            throw new DataNotFoundException("member not found");
-        }
 
-    }
 }
