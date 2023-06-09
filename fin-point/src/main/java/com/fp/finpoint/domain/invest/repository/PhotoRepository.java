a1cea7f82778fe7a8e37483a34765f597fb70bd5
hojncode <glicm12@gmail.com>
2023-06-09 16:33:17 +0900

    REMOVE : 불필요 파일 삭제

--- fin-point/src/main/java/com/fp/finpoint/domain/invest/repository/PhotoRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.fp.finpoint.domain.invest.repository;
-//
-//import com.fp.finpoint.domain.invest.entity.PhotoDto;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface PhotoRepository extends JpaRepository<PhotoDto, Long> {
-//}
