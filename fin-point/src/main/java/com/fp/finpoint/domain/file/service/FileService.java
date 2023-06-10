624cc693ed09655b2ade6dbcbda8d98de4927043
qsdfdsf <xivpark1110@gmail.com>
2023-06-10 21:10:15 +0900

    FIX   : nypage 수정

--- fin-point/src/main/java/com/fp/finpoint/domain/file/service/FileService.java
+++ fin-point/src/main/java/com/fp/finpoint/domain/file/service/FileService.java
@@ -6,7 +6,6 @@ import com.fp.finpoint.domain.file.repository.FileRepository;
 import groovy.util.logging.Slf4j;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,47 +18,26 @@ import java.util.UUID;
 @Service
 public class FileService {
 
-    @Value("${file.dir}")
+    @Value("${file.dir}") // file: dir: c:/test/ 같이 yml 추가 및 경로에 폴더 필요
     private String fileDirectory;
-
-    private final ResourceLoader resourceLoader;
-
     private final FileRepository fileRepository;
 
     public Long saveFile(MultipartFile files) throws IOException {
-
         if (files.isEmpty()) {
             return null;
         }
-
-        // 원래 파일 이름 추출
         String origName = files.getOriginalFilename();
-
-        // 파일 이름으로 쓸 uuid 생성
         String uuid = UUID.randomUUID().toString();
-
-        // 확장자 추출(ex : .png)
         String extension = origName.substring(origName.lastIndexOf("."));
-
-        // uuid와 확장자 결합
         String savedName = uuid + extension;
-
-        // 파일을 불러올 때 사용할 파일 경로
         String savedPath = fileDirectory + savedName;
-
-        // 파일 엔티티 생성
         FileEntity file = FileEntity.builder()
                 .originName(origName)
                 .savedName(savedName)
                 .savedPath(savedPath)
                 .build();
-
-        // 실제로 로컬에 uuid를 파일명으로 저장
         files.transferTo(new File(savedPath));
-
-        // 데이터베이스에 파일 정보 저장
         FileEntity savedFile = fileRepository.save(file);
-
         return savedFile.getId();
     }
 
--- fin-point/src/main/resources/static/mypage/js/mypage.js
+++ fin-point/src/main/resources/static/mypage/js/mypage.js
@@ -10,7 +10,29 @@
             window.open(data, "인증", "width=800px, height=800px");
         })
 }
-
     function openProfileWindow() {
     window.open("/profile", '프로필 이미지 변경', 'width=400,height=300');
 }
+
+    function confirmProfileImage() {
+        const fileInput = document.getElementById('profile-input');
+        if (fileInput.files && fileInput.files[0]) {
+            const formData = new FormData();
+            formData.append('profileImage', fileInput.files[0]);
+
+            fetch('/update-profile', {
+                method: 'POST',
+                body: formData
+            })
+                .then(response => response.json())
+                .then(data => {
+                    alert(data.message)
+                    window.close()
+                })
+                .catch(error => {
+                    console.error('이미지 업데이트 실패:', error);
+                });
+        } else {
+            console.log('이미지를 선택하세요');
+        }
+    }
