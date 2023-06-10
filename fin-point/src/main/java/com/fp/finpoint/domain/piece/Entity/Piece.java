fd3da7745824c707d3f626f4ea26e65d337b05a9
Damgom <wisewish1@gmail.com>
2023-06-10 16:38:06 +0900

    REFACTOR: add new column in piece

--- fin-point/src/main/java/com/fp/finpoint/domain/piece/Entity/Piece.java
+++ fin-point/src/main/java/com/fp/finpoint/domain/piece/Entity/Piece.java
@@ -1,5 +1,6 @@
 package com.fp.finpoint.domain.piece.Entity;
 
+import com.fp.finpoint.domain.invest.entity.Invest;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,15 +14,17 @@ public class Piece {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String name;
     private Long price;
     private Long count;
-    //todo: member와 1:n, Invest(판매글)과 1:1
-    //todo: 동시성 이슈가 생길 수 있음 낙관적 락을 걸고 일단 테스트 진행
     @Version
     private Long version;
 
+    @OneToOne(mappedBy = "piece")
+    private Invest invest;
 
-    public Piece(Long price, Long count) {
+    public Piece(String name, Long price, Long count) {
+        this.name = name;
         this.price = price;
         this.count = count;
     }
