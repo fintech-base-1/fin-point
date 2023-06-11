2e7ab98ec0b3b610273e6aef5712143c5f259580
hojncode <glicm12@gmail.com>
2023-06-11 15:33:53 +0900

    FEAT : 게시글 이미지 업로드 기능 구현 (servlet 버젼)

--- fin-point/src/main/java/com/fp/finpoint/web/invest/controller/InvestController.java
+++ fin-point/src/main/java/com/fp/finpoint/web/invest/controller/InvestController.java
@@ -4,17 +4,29 @@ import com.fp.finpoint.domain.invest.entity.Invest;
 import com.fp.finpoint.domain.invest.entity.InvestDto;
 import com.fp.finpoint.domain.invest.service.InvestService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StreamUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @RequestMapping("/invest")
 @Controller
 @RequiredArgsConstructor // DI 주입. (InvestService)
@@ -56,13 +68,49 @@ public class InvestController {
     }
 
     // 글 작성.
+    @Value("${file.dir}") // @Value 는 application.yml 파일에 입력되어있는 값을 가져올수 있다.
+    private String fileDir;
+
     @GetMapping("/create")
     public String listCreate(InvestDto investDto) {
         return "invest_create";
     }
 
     @PostMapping("/create")
-    public String listCreate(Model model, InvestDto investDto) {
+    public String listCreate(Model model, InvestDto investDto , HttpServletRequest httpServletRequest) throws ServletException, IOException {
+        log.info("request={}", httpServletRequest);
+
+        String itemName = httpServletRequest.getParameter("itemName");
+        log.info("itemName={}", itemName);
+
+        Collection<Part> parts = httpServletRequest.getParts();
+        log.info("parts={}", parts);
+
+        for (Part part : parts) {
+            log.info("=== PART ===");
+            log.info("name={}", part.getName());
+            Collection<String> headerNames = part.getHeaderNames(); // part 는 헤더와 바디로 구분 되어있다.
+            for (String headerName : headerNames) {
+                log.info("header {}:{}", headerName, part.getHeader(headerName));
+            }
+            //편의 메서드로 제공된,
+            //content-disposition 를 이용.
+            log.info("submittedFilename={}", part.getSubmittedFileName());
+            log.info("size={}", part.getSize()); // part 의 body size
+
+            //데이터 읽기.
+            InputStream inputStream = part.getInputStream();
+            String body = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+            log.info("body={}", body);
+
+            //파일에 저장하기.
+            if (StringUtils.hasText(part.getSubmittedFileName())) {
+               String fullPath = fileDir + part.getSubmittedFileName();
+               log.info("파일 저장 fullPath={}",fullPath);
+               part.write(fullPath);
+            }
+        }
+
         this.investService.create(investDto);
         return "redirect:/invest/list";
     }
