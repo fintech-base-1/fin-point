fdc1f7f858e0c273fa0d40fe9d9a99a95a582bef
arxivj <xivpark1110@gmail.com>
2023-06-14 02:35:53 +0900

    FIX : change path

--- fin-point/src/main/java/com/fp/finpoint/web/ranking/controller/RankingController.java
+++ fin-point/src/main/java/com/fp/finpoint/web/ranking/controller/RankingController.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Slf4j
 @Controller
 @RequiredArgsConstructor
-//@RequestMapping("/finpoint")
+@RequestMapping("/finpoint")
 public class RankingController {
 
     private final RankingService rankingService;
